d1a88dfdbe77e1cf5e6d01c1b95e22796fa3571c
scurest <scurest@users.noreply.github.com>
2017-08-14 23:08:53 -0500

    Messy armature implementation
    We can't pose or animate an object hierarchy directly, so instead
    create an armature that mirrots the object tree. Use constraints
    to force objects to sit at the location of the corresponding bone.

--- io_scene_gltf/node.py
+++ io_scene_gltf/node.py
@@ -1,8 +1,6 @@
 import bpy
-from mathutils import Matrix
-from mathutils import Quaternion
-from mathutils import Vector
-
+import math
+from mathutils import Matrix, Quaternion, Vector
 
 def convert_matrix(m):
     """Convert glTF matrix to Blender matrix"""
@@ -16,9 +14,9 @@ def convert_quaternion(q):
     return Quaternion([q[3], q[0], q[1], q[2]]) # xyzw -> wxyz
 
 
-def set_transform(obj, node):
+def get_transform(node):
     if 'matrix' in node:
-        obj.matrix_local = convert_matrix(node['matrix'])
+        return convert_matrix(node['matrix'])
     else:
         mat = Matrix()
         if 'scale' in node:
@@ -35,13 +33,18 @@ def set_transform(obj, node):
         if 'translation' in node:
             t = Vector(node['translation'])
             mat = Matrix.Translation(t) * mat
-        obj.matrix_local = mat
+        return mat
 
 
-def create_object(op, idx, parent, scene):
+def create_object(op, idx, parent, scene, armature, node_to_bone_map):
     node = op.root['nodes'][idx]
     name = node.get('name', 'nodes[%d]' % idx)
     ob = bpy.data.objects.new(name, None)
+    ob.empty_draw_size = 0.2
+
+    con = ob.constraints.new('COPY_TRANSFORMS')
+    con.target = armature
+    con.subtarget = node_to_bone_map[idx]
 
     if 'mesh' in node:
         mesh_ob = bpy.data.objects.new(
@@ -58,8 +61,7 @@ def create_object(op, idx, parent, scene):
         camera_ob.parent = ob
         scene.objects.link(camera_ob)
 
-    set_transform(ob, node)
-
+    ob.matrix_local = get_transform(node)
     ob.parent = parent
     bpy.context.scene.objects.link(ob)
     scene.update()
@@ -67,7 +69,53 @@ def create_object(op, idx, parent, scene):
     if 'children' in node:
         children = node['children']
         for idx in children:
-            create_object(op, idx, ob, scene)
+            create_object(op, idx, ob, scene, armature, node_to_bone_map)
+
+
+def create_tree(op, root_idx, scene):
+    root_node = op.root['nodes'][root_idx]
+    name = root_node.get('name', 'node[%d]' % root_idx)
+
+    bpy.ops.object.add(
+        type='ARMATURE',
+        enter_editmode=True,
+        location=(0,0,0))
+    ob = bpy.context.object
+    ob.name = name
+    amt = ob.data
+    amt.name = name + '.AMT'
+
+    node_to_bone_map = {}
+
+    bpy.ops.object.mode_set(mode='EDIT')
+
+    def add_bone(idx, parent, parent_mat):
+        node = op.root['nodes'][idx]
+        name = node.get('name', 'node[%d]' % idx)
+        # Urg, isn't this backwards from get_transform? Figure out why.
+        mat = parent_mat * get_transform(node)
+
+        bone = amt.edit_bones.new(name)
+        bone.use_connect = False
+        if parent:
+            bone.parent = parent
+        bone.head = mat * Vector((0,0,0))
+        #TODO use heuristic for bone length
+        bone.tail = mat * Vector((0,0.2,0))
+        bone.align_roll(mat * Vector((0,0,1)) - bone.head)
+        #NOTE bones don't seem to have non-uniform scaling.
+        # Maybe we can do something with the bind pose...
+
+        node_to_bone_map[idx] = bone.name
+
+        for child_idx in node.get('children', []):
+            add_bone(child_idx, bone, mat)
+
+    add_bone(root_idx, None, Matrix())
+
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    create_object(op, root_idx, None, scene, ob, node_to_bone_map)
 
 
 def create_scene(op, idx):
@@ -82,7 +130,7 @@ def create_scene(op, idx):
 
     roots = scene.get('nodes', [])
     for root_idx in roots:
-        create_object(op, root_idx, None, scn)
+        create_tree(op, root_idx, scn)
 
     scn.update()
 
