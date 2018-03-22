057e3df1d44223ed20c49c6d367d1a2259141905
David Mugnai <dvd@gnx.it>
2018-03-22 17:40:22 +0100

    configure the camera from the gltf data

--- addons/io_scene_gltf/camera.py
+++ addons/io_scene_gltf/camera.py
@@ -2,7 +2,23 @@ import bpy
 
 
 def create_camera(op, idx):
-    camera = op.gltf['cameras'][idx]
-    name = camera.get('name', 'cameras[%d]' % idx)
-    data = bpy.data.cameras.new(name)
-    return data
+    """
+    create a new blender camera from the gltf data
+    """
+    data = op.gltf['cameras'][idx]
+    name = data.get('name', 'cameras[%d]' % idx)
+    camera = bpy.data.cameras.new(name)
+    if data["type"] == "orthographic":
+        camera.type = "ORTHO"
+        p = data["orthographic"]
+        camera.clip_start = p["znear"]
+        camera.clip_end = p["zfar"]
+        camera.ortho_scale = max(p["xmag"], p["ymag"])
+    else:
+        camera.type = "PERSP"
+        p = data["perspective"]
+        camera.clip_start = p["znear"]
+        camera.clip_end = p["zfar"]
+        camera.lens_unit = "FOV"
+        camera.angle_x = p["yfov"] * p["aspectRatio"]
+    return camera
