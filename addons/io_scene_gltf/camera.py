d0d955b47a901e03cb9fe725324b2f787bd0f842
David Mugnai <dvd@gnx.it>
2018-03-26 10:07:53 +0200

    camera/create_camera: fallback behavior to handle a missing apesctRatio

--- addons/io_scene_gltf/camera.py
+++ addons/io_scene_gltf/camera.py
@@ -22,7 +22,17 @@ def create_camera(op, idx):
         # according to the spec a missing zfar means "infinite"
         camera.clip_end = p.get("zfar", math.inf)
         camera.lens_unit = "FOV"
-        camera.angle_x = p["yfov"] * p["aspectRatio"]
+
+        # The aspectRatio is optional and if missing the importer must provide
+        # a "sensible" default.
+        # Since angle_x and angle_y depend on each other, if the apsect raio is
+        # missing, I let blender figure out the other valus using it's internal
+        # settings.
+        aspectRatio = p.get("aspectRatio")
+        if aspectRatio:
+            camera.angle_x = p["yfov"] * aspectRatio
+        else:
+            camera.angle_y = p["yfov"]
     else:
         # this branch should never been taken since the only camera types are
         # "orthographic" and "perspective"; but if the input document use an
