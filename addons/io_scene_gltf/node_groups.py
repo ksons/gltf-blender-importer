3df3dcb78f834819f7569643a4437bdab7e0b333
scurest <scurest@users.noreply.github.com>
2018-10-09 01:21:21 -0500

    Node group serialization improvements
    Taught the serializer about Mapping nodes which will be used in
    the next commit. The process of editing the groups was made
    simpler (see the comment). node_groups.json will be more line-
    based from now on too; one line per group. This makes diffs
    on it a little nicer.

--- addons/io_scene_gltf/node_groups.py
+++ addons/io_scene_gltf/node_groups.py
@@ -1,19 +1,17 @@
 import json, os
 import bpy
 
-
 # Load the serialized group data. Serialized data comes from
 # KhronosGroup/glTF-Blender-Exporter/pbr_node/glTF2.blend, plus some
 # modifications.
 this_dir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(this_dir, 'node_groups.json'), 'r') as f:
+node_groups_path = os.path.join(this_dir, 'node_groups.json')
+with open(node_groups_path, 'r') as f:
     f.readline() # throw away comment line
     GROUP_DATA = json.load(f)
 
-
 def create_group(op, name):
     data = GROUP_DATA[name]
-
 
     # Before we create a new one, if there is an existing group with the right
     # name and whose inputs/outputs have the right names, (perhaps from a
@@ -29,7 +27,6 @@ def create_group(op, name):
         if matches:
             return g
 
-
     g = bpy.data.node_groups.new(data['name'], 'ShaderNodeTree')
     inputs = g.inputs
     outputs = g.outputs
@@ -39,7 +36,6 @@ def create_group(op, name):
     # New groups aren't empty; empty it
     while nodes:
         nodes.remove(nodes[0])
-
 
     def deserialize_sockets(sockets, ys):
         for y in ys:
@@ -51,15 +47,15 @@ def create_group(op, name):
     deserialize_sockets(inputs, data['inputs'])
     deserialize_sockets(outputs, data['outputs'])
 
-
     for y in data['nodes']:
         node = nodes.new(y['idname'])
         node.name = y['name']
-        if 'label' in y: node.label = y['label']
-        if 'operation' in y: node.operation = y['operation']
-        if 'blend_type' in y: node.blend_type = y['blend_type']
-        if 'use_clamp' in y: node.use_clamp = y['use_clamp']
         if 'node_tree' in y: node.node_tree = op.get('node_group', y['node_tree'])
+        for attr in [
+            'label', 'operation', 'blend_type', 'use_clamp',
+            'translation', 'rotation', 'scale'
+        ]:
+            if attr in y: setattr(node, attr, y[attr])
 
         for i, v in enumerate(y['inputs']):
             if v != None: node.inputs[i].default_value = v
@@ -75,20 +71,18 @@ def create_group(op, name):
         nodes[i].width = y['width']
         nodes[i].height = y['height']
 
-
     for i in range(0, len(data['links']), 4):
         a, b, c, d = data['links'][i:i+4]
         links.new(nodes[a].outputs[b], nodes[c].inputs[d])
 
-
     return g
 
 
-# NOTE: Not part of the importer. When run as a script inside Blender, imports
-# all the serialized node groups. Can be used to edit the serialized groups by
-# running this in an empty file, editing, then running the serialize_node_groups
-# script. You'll have to set this_dir above manually though.
-if __name__ == '__main__':
+# The rest of this file isn't used in the importer but you can use it to edit
+# the serialized groups. First run load() to load all the groups, edit, and then
+# serialize them back to node_groups.json with serialize().
+
+def load():
     # Implements *just* enough of ImportGLTF to get create_group to work :)
     class ProxyOp:
         def __init__(self):
@@ -103,3 +97,100 @@ if __name__ == '__main__':
     op = ProxyOp()
     for name in GROUP_DATA.keys():
         create_group(op, name)
+
+def serialize_group(group):
+    def val(x):
+        if type(x) == str: return x
+        if hasattr(x, '__len__'): return list(x)
+        return x
+
+    def serialize_sockets(sockets):
+        result = []
+        for s in sockets:
+            x = {
+                'name': s.name,
+                'idname': s.bl_socket_idname,
+            }
+            if hasattr(s, 'default_value'): x['default_value'] = val(s.default_value)
+            if hasattr(s, 'min_value'): x['min_value'] = val(s.min_value)
+            if hasattr(s, 'max_value'): x['max_value'] = val(s.max_value)
+            result.append(x)
+        return result
+
+    inputs = serialize_sockets(group.inputs)
+    outputs = serialize_sockets(group.outputs)
+
+    node_to_idx = {}
+    for i, node in enumerate(group.nodes):
+        node_to_idx[node] = i
+
+    nodes = []
+    for node in group.nodes:
+        x = {
+            'name': node.name,
+            'idname': node.bl_idname,
+            'location': val(node.location),
+            'width': node.width,
+            'height': node.height,
+            'inputs': [],
+            'outputs': [],
+        }
+
+        if node.parent: x['parent'] = node_to_idx[node.parent]
+        if hasattr(node, 'label') and node.label != '': x['label'] = node.label
+        if hasattr(node, 'node_tree'): x['node_tree'] = node.node_tree.name
+
+        for attr in [
+            'operation', 'blend_type', 'use_clamp',
+            'translation', 'rotation', 'scale',
+        ]:
+            if hasattr(node, attr):
+                x[attr] = val(getattr(node, attr))
+
+        for input in node.inputs:
+            if input.links or not hasattr(input, 'default_value'):
+                x['inputs'].append(None)
+            else:
+                x['inputs'].append(val(input.default_value))
+        for output in node.outputs:
+            if output.links or not hasattr(output, 'defaultvalue'):
+                x['outputs'].append(None)
+            else:
+                x['outputs'].append(val(output.default_value))
+
+        nodes.append(x)
+
+    links = []
+    for link in group.links:
+        from_node_id = node_to_idx[link.from_node]
+        from_socket_id = list(link.from_node.outputs).index(link.from_socket)
+        to_node_id = node_to_idx[link.to_node]
+        to_socket_id = list(link.to_node.inputs).index(link.to_socket)
+        links += [from_node_id, from_socket_id, to_node_id, to_socket_id]
+
+    return {
+        'name': group.name,
+        'inputs': inputs,
+        'outputs': outputs,
+        'nodes': nodes,
+        'links': links,
+    }
+
+def serialize():
+    groups = {}
+    for group in bpy.data.node_groups:
+        groups[group.name] = serialize_group(group)
+
+    with open(node_groups_path, 'w') as f:
+        f.write('// !!AUTO-GENERATED!! See serialize_node_groups.py\n')
+        f.write('{\n')
+        keys = list(groups.keys())
+        keys.sort()
+        for k in keys:
+            json.dump(k, f)
+            f.write(':')
+            json.dump(groups[k], f, separators=(',',':'))
+            if k != keys[-1]:
+                f.write(',')
+            f.write('\n')
+        f.write('}')
